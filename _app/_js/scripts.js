91d46f3405431abadd5e836612a5c30f2b0e3c40
pavelp21 <pavelvp2182@yandex.ru>
2020-02-02 00:25:11 +0500

    Mainpage is stylezed

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -2,6 +2,15 @@
 //@prepros-prepend bootstrap.min.js
 //@prepros-prepend jquery.fancybox.min.js
 
+var isIE = false || !!document.documentMode;
+
+if (isIE) {
+    var head = document.getElementsByTagName("head")[0];
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "css/ie.min.css";
+    head.appendChild(link);
+}
 $(document).ready(function () {
     $(function () {
         var check = $('.check', this),
@@ -113,19 +122,6 @@ $(document).ready(function () {
         var a = $(this).parent().attr("data-youtube");
         $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1"  allowfullscreen></iframe>')
     });
-    $(function () {
-        $('#video-revs').on('slide.bs.carousel', function () {
-            $('.video-wrapper-rev iframe').each(function () {
-                var l = $(this).parent().attr('data-img');
-                $(this).parent().html('<img src="' + l + '" alt="Видео отзыв">');
-            });
-            $(".video-wrapper-rev img").click(function () {
-                var a = $(this).parent().attr("data-youtube");
-                $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1"  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>');
-            });
-        });
-    });
-
 
     /*Конец документа*/
 });
